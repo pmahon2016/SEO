84d3aae207e97aaa29f268fe91b71792818f7a99
Paul Mahon <paulmahon@me.com>
2025-11-15 12:08:45 +0000

    updated logo

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def analyze():
 
 if __name__ == '__main__':
     # For local development only
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5004)
 
